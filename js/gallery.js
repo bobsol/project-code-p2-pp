9d1deeb133988a9586efef537944c52d2748f04c
bobsol <bobsol@gmail.com>
2016-04-23 22:34:07 +0000

    updates as per Sheldon, test on mobile

--- js/gallery.js
+++ js/gallery.js
@@ -1,13 +1,37 @@
 $(document).ready(function($) {
-    $("#gallery .col-md-2 img").hover(function() {
-        $("#gallery .col-md-2 img").removeClass('change');
+    $("#gallery .col-sm-2 img").hover(function() {
+        $("#gallery .col-sm-2 img").removeClass('change');
         $(this).addClass('change');
         $('#main-img').attr('src',$(this).attr('src'));
     });
-    $("#gallery .col-md-2 img").mouseenter(function() {
+    $("#gallery .col-sm-2 img").mouseenter(function() {
         $(this).addClass("glow");
     });
-    $("#gallery .col-md-2 img").mouseleave(function() {
+    $("#gallery .col-sm-2 img").mouseleave(function() {
+        $(this).removeClass("glow");
+    });
+    /* these are for the shadows on the tabbed box page */
+    $(".shadow img").mouseenter(function() {
+        $(this).addClass("glow");
+    });
+    $(".shadow img").mouseleave(function() {
         $(this).removeClass("glow");
     });
 });
+
+/* change classes based on viewport, from here:
+   https://www.sitepoint.com/community/t/adding-removing-classes-on-resize-jquery/191805/3 
+   with a little tinker from me */
+$(window).on('load, resize', function mobileViewUpdate() {
+    var viewportWidth = $(window).width();
+    if (viewportWidth < 500) {
+        $(".btn-group").removeClass("btn-group").addClass("btn-group-xs")
+    }else{
+        $(".btn-group-xs").removeClass("btn-group-xs").addClass("btn-group")
+    }
+});
+
+// disappear buttons
+function bPress(which) {
+    which.style.visibility = "hidden";
+}
